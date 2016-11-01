2c348b2d25f58946b4742c197f4baf915e3cb027
vasigorc <vasigorc@yahoo.ca>
2016-10-31 22:22:38 -0400

    implemented: update(Inpatient i) and  updateBatch(List<Inpatient> entities) in InpatientRepositoryJDBC. Two tests added

--- src/main/java/ca/vgorcinschi/dao/repositories/InpatientRepositoryJDBC.java
+++ src/main/java/ca/vgorcinschi/dao/repositories/InpatientRepositoryJDBC.java
@@ -2,14 +2,21 @@ package ca.vgorcinschi.dao.repositories;
 
 import static ca.vgorcinschi.CommonUtil.localToSql;
 import ca.vgorcinschi.model.Inpatient;
+import java.math.BigDecimal;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Arrays;
 import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionTemplate;
 
 /**
  * Inpatient implementation of GenericRepository and DetailRecordMethods
@@ -31,10 +38,14 @@ public class InpatientRepositoryJDBC implements InpatientRepository {
     //configured in src.main.resources.application.yml
     private final JdbcTemplate jdbcTemplate;
 
+    //Spring's transaction template is used to rollback the transaction
+    private final TransactionTemplate transactionTemplate;
+
     //injection point
     @Autowired
-    public InpatientRepositoryJDBC(JdbcTemplate jdbcTemplate) {
+    public InpatientRepositoryJDBC(JdbcTemplate jdbcTemplate, TransactionTemplate transactionTemplate) {
         this.jdbcTemplate = jdbcTemplate;
+        this.transactionTemplate = transactionTemplate;
     }
 
     @Override
@@ -52,11 +63,11 @@ public class InpatientRepositoryJDBC implements InpatientRepository {
             jdbcTemplate.update(sql, entity.getPatientId(), localToSql.apply(entity.getDateOfStay()),
                     entity.getRoomNumber(), entity.getDailyRate(), entity.getSupplies(),
                     entity.getServices());
-            log.info("Patient " + entity + " was successfully saved to the DB.");
+            log.info("Inpatient " + entity + " was successfully saved to the DB.");
             return true;
         } catch (DataAccessException e) {
             //log error and return false
-            log.error("SQL query for adding Patient " + entity + " failed. "
+            log.error("SQL query for adding Inpatient " + entity + " failed. "
                     + e.getMostSpecificCause().toString());
             return false;
         }
@@ -64,17 +75,124 @@ public class InpatientRepositoryJDBC implements InpatientRepository {
 
     @Override
     public boolean update(Inpatient entity) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if (entity.getId() > 0) {
+            /*
+             creating a new controlled transaction. obtain a callback function
+             (new TransactionCallback<Boolean>(TransactionStatus)) from the 
+             transaction template. This function takes only one argument - Transaction
+             status which can rollback a transaction. Using lambda we avoid the
+             explicit new TransactionCallback<Boolean>(TransactionStatus) declaration
+             */
+            return transactionTemplate.execute((TransactionStatus transactionStatus) -> {
+                //prepared statement
+                String updateStatement = "UPDATE INPATIENT SET PATIENTID = ?,"
+                        + " DATEOFSTAY=?, ROOMNUMBER=?, DAILYRATE=?, SUPPLIES=? "
+                        + ",SERVICES =? WHERE ID=?";
+                //query arguments, jdbcTemplate will automatically cast the types
+                Object[] args = {entity.getPatientId(), localToSql.apply(entity.getDateOfStay()),
+                    entity.getRoomNumber(), entity.getDailyRate(), entity.getSupplies(),
+                    entity.getServices(), entity.getId()};
+                //update the db, return the # of affected rows
+                if (jdbcTemplate.update(updateStatement, args) == 1) {
+                    log.info("Inpatient successfully updated in the database: " + entity);
+                    return true;
+                }
+                //if other then one row was affected roll back transaction
+                //and log error
+                transactionStatus.setRollbackOnly();
+                log.error("Couldn't update " + entity + ". Query tried to modify "
+                        + "more/less then one row. Check db design.");
+                return false;
+            });
+        } else {
+            //log error and return false
+            log.error(entity + " doesn't have a valid id. Persistance skipped.");
+            return false;
+        }
     }
 
     @Override
     public boolean delete(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if (id > 0) {
+            /*
+             creating a new controlled transaction. obtain a callback function
+             (new TransactionCallback<Boolean>(TransactionStatus)) from the 
+             transaction template. This function takes only one argument - Transaction
+             status which can rollback a transaction. Using lambda we avoid the
+             explicit new TransactionCallback<Boolean>(TransactionStatus){} declaration
+             */
+            return transactionTemplate.execute((TransactionStatus transactionStatus) -> {
+                String deleteStatement = "DELETE FROM INPATIENT WHERE ID = ?";
+                //update the db, return the # of deleted rows
+                if (jdbcTemplate.update(deleteStatement, id) == 1) {
+                    log.info("Inpatient with id " + id + " has been removed from "
+                            + "the database");
+                    return true;
+                }
+                log.error(id + " isn't a valid id. Deleting skipped.");
+                return false;
+            });
+        } else {
+            log.error("Cannot delete the inpatient: " + id + " is an invalid "
+                    + "id");
+            return false;
+        }
     }
 
     @Override
     public boolean updateBatch(List<Inpatient> entities) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //if there's nothing to update - return false
+        if (entities.isEmpty()) {
+            log.warn("updateBatch() was called on an empty Inpatients list.");
+            return false;
+        } else {
+            //view comments for the block in lines 117-123
+            return transactionTemplate.execute((TransactionStatus transactionStatus) -> {
+                //prepared statement that will be used for updating each record
+                String sql = "UPDATE INPATIENT SET PATIENTID = ?,"
+                        + " DATEOFSTAY=?, ROOMNUMBER=?, DAILYRATE=?, SUPPLIES=? "
+                        + ",SERVICES =? WHERE ID=?";
+                /**
+                 * a couple of notes about batchUpdate() method. It executes the
+                 *
+                 * @param sql on every item in the entities
+                 * @return an array of integers (where integers is number of
+                 * rows affected) by each query
+                 */
+                int[] arrayOfaffectedRows = jdbcTemplate.batchUpdate(sql, new BatchPreparedStatementSetter() {
+                    @Override
+                    public int getBatchSize() {
+                        return entities.size();
+                    }
+
+                    @Override
+                    public void setValues(PreparedStatement ps, int i) throws SQLException {
+                        Inpatient inpatient = entities.get(i);
+                        ps.setInt(1, inpatient.getPatientId());
+                        ps.setTimestamp(2, localToSql.apply(inpatient.getDateOfStay()));
+                        ps.setString(3, inpatient.getRoomNumber());
+                        ps.setBigDecimal(4, inpatient.getDailyRate());
+                        ps.setBigDecimal(5, inpatient.getSupplies());
+                        ps.setBigDecimal(6, inpatient.getServices());
+                        ps.setInt(7, inpatient.getId());
+                    }
+                });
+                /*
+                 Our test is "Did every query update only one row in the DB?"
+                 if yes - it is good, else we will rollback.
+                 */
+                if (Arrays.stream(arrayOfaffectedRows).allMatch(i -> i == 1)) {
+                    log.info(entities.size() + " inpatient records for patient "
+                            + "with id " + entities.get(0).getPatientId() + " have been successfully "
+                            + "updated.");
+                    return true;
+                }
+                log.error("Rolled back batch update of " + entities.size() + " records for patient "
+                        + "with id " + entities.get(0).getPatientId() + " for the attempt "
+                        + "to modify <>1 record(s) with a single update statement");
+                return false;
+            });
+        }
     }
 
     @Override
--- src/test/java/ca/vgorcinschi/dao/repositories/InpatientRepositoryTests.java
+++ src/test/java/ca/vgorcinschi/dao/repositories/InpatientRepositoryTests.java
@@ -9,7 +9,10 @@ import ca.vgorcinschi.model.Inpatient;
 import static java.math.BigDecimal.*;
 import static java.time.LocalDateTime.*;
 import java.time.Month;
+import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import org.junit.After;
 import static org.junit.Assert.*;
 import org.junit.Before;
@@ -28,10 +31,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class InpatientRepositoryTests {
-    
+
     @Autowired
     private JdbcTemplate jdbcTemplate;
-    
+
     @Autowired
     private InpatientRepository repository;
 
@@ -40,17 +43,17 @@ public class InpatientRepositoryTests {
 
     //random used in the setUp() method. It is cheaper to reuse one Random object
     private final Random random;
-    
+
     public InpatientRepositoryTests() {
         random = new Random(System.currentTimeMillis());
     }
-    
+
     @Before
     public void setUp() {
         inpatient = new Inpatient();
         //assign to a random patient with id 1-20
         //source=http://stackoverflow.com/questions/363681/generating-random-integers-in-a-specific-range
-        int id = random.nextInt((16 - 1) + 1) + 1;
+        int id = random.nextInt((26 - 1) + 1) + 1;
         inpatient.setPatientId(id);
         inpatient.setDailyRate(valueOf(10));
         inpatient.setDateOfStay(of(2016, Month.MARCH, 15, 10, 00));
@@ -58,18 +61,63 @@ public class InpatientRepositoryTests {
         inpatient.setServices(valueOf(15));
         inpatient.setSupplies(valueOf(5));
     }
-    
+
     @After
     public void tearDown() {
     }
-    
+
     @Test
     public void inpatientRepoIsNotNull() {
         assertNotNull(repository);
     }
-    
+
+    /*
+     Ignoring for now - too many created. To increase the chance of a successfull
+     test. Increase the range in line 56
+     */
     @Test
+    @Ignore
     public void addInpatientTest() {
         assertTrue(repository.add(inpatient));
+    }
+
+    @Test
+    public void updateInpatientTest() {
+        /*
+         We don't have and we don't need a method for retrieving an 
+         inpatient by id or name. We will manually 'hard' set the id
+         to an existing id in the db and try to update it.
+         */
+        inpatient.setId(7);
+        assertTrue(repository.update(inpatient));
+    }
+
+    //skipping the test to not delete too many
+    @Test
+    @Ignore
+    public void deleteInpatientTest() {
+        /*
+         random int > 5 (don't want to delete our default values) but less then 15
+         source=http://stackoverflow.com/questions/363681/generating-random-integers-in-a-specific-range
+         */
+        int id = random.nextInt((22 - 5) + 1) + 5;
+        assertTrue(repository.delete(id));
+    }
+
+    @Test
+    public void batchUpdateInpatienstTest() {
+        /*
+         The test lies in creating a list of inpatients with existing ids
+         (note that) id of the patient isn't important and passing it to
+         the repository's method. With the help of an IntStream we will
+         collect a list of inpatients with ids between 5 and 11
+         */
+        List<Inpatient> inpatients = IntStream.rangeClosed(5, 11).boxed()
+                .map((Integer i) -> {
+                    inpatient.setId(i);
+                    return inpatient;
+                }).collect(Collectors.toList());
+        //theoretically our test should return true
+        assertTrue(repository.updateBatch(inpatients));
     }
 }
