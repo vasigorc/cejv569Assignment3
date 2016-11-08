9993126967cacd4b7b8b77e1f08c9744bd6f49ab
vasigorc <vasigorc@yahoo.ca>
2016-11-07 22:01:40 -0500

    Added patientDbService interface

---
+++ src/main/java/ca/vgorcinschi/dao/repositories/PatientDBService.java
@@ -0,0 +1,29 @@
+package ca.vgorcinschi.dao.repositories;
+
+import ca.vgorcinschi.model.Patient;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * DAO interface that will be used by the client
+ *
+ * @author vgorcinschi
+ */
+public interface PatientDBService {
+
+    public boolean addPatient(Patient p);
+
+    public boolean addDetailRecord(Object detailRecord);
+
+    public boolean updateDetailRecord(Object detailRecord);
+
+    public boolean deleteDetailRecord(Object detailRecord);
+
+    public Optional<Patient> findById(int id);
+
+    public Optional<List<Patient>> findByName();
+
+    public Optional<List<Patient>> allPatients();
+
+    public boolean deletePatient();
+}
