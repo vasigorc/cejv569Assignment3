53b83137e953204c38f63596958d2d94c83f3ffd
v_gorcin <v_gorcin@CCE-LAB107-62.concordia.ca>
2016-11-09 20:03:09 -0500

    implemented and tested addDetailRecord() in the service

--- src/test/java/ca/vgorcinschi/dao/PatientDBServiceTests.java
+++ src/test/java/ca/vgorcinschi/dao/PatientDBServiceTests.java
@@ -1,8 +1,11 @@
 package ca.vgorcinschi.dao;
 
 import ca.vgorcinschi.dao.repositories.PatientRepository;
+import ca.vgorcinschi.model.Medication;
 import ca.vgorcinschi.model.Patient;
+import static java.math.BigDecimal.valueOf;
 import java.time.LocalDateTime;
+import java.util.Random;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.After;
 import static org.junit.Assert.*;
@@ -54,17 +57,31 @@ public class PatientDBServiceTests {
     }
 
     @Test
-    public void insertPatient() {
+    public void insertPatientTest() {
         assertTrue(service.savePatient(patient));
     }
 
     @Test
-    public void updatePatient() {
+    public void updatePatientTest() {
+        //generating a "custom" random last name to retrieve it back
         String checkName = RandomStringUtils.randomAlphabetic(8);
         patient.setLastName(checkName);
         service.savePatient(patient);
         Patient justInserted = patientRepository.findByLastName(checkName).get(0);
         justInserted.setLastName("I have just been updated!");
         assertTrue(justInserted.getPatientId() > 0 && service.savePatient(justInserted));
+    }
+
+    @Test
+    public void addDetailRecordTest() {
+        //creating a sample detail record
+        Medication medication = new Medication();
+        int id = new Random().nextInt((5 - 1) + 1) + 1;
+        medication.setPatientId(id);
+        medication.setDateOfMedication(LocalDateTime.now().minusDays(22));
+        medication.setMed("Medication");
+        medication.setUnitCost(valueOf(15));
+        medication.setUnits(valueOf(5));
+        assertTrue(service.addDetailRecord(medication));
     }
 }
--- src/test/java/ca/vgorcinschi/dao/repositories/MedicationRepositoryTests.java
+++ src/test/java/ca/vgorcinschi/dao/repositories/MedicationRepositoryTests.java
@@ -21,7 +21,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.dao.DataAccessException;
 import org.springframework.test.context.junit4.SpringRunner;
 
 /**
