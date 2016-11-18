21c44d7a692a5450dae5611583521f7c3b314e12
vasigorc <vasigorc@yahoo.ca>
2016-11-17 19:46:39 -0500

    Added basic spring/javafx support by Thomas Darimont

---
+++ src/main/java/ca/vgorcinschi/App.java
@@ -0,0 +1,37 @@
+package ca.vgorcinschi;
+
+import ca.vgorcinschi.ui.MainLayout;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Lazy;
+
+@SpringBootApplication
+@Lazy
+public class App extends AbstractJavaFxApplicationSupport{
+
+	/**
+	 * Note that this is configured in application.properties
+	 */
+	@Value("${app.ui.title:Example App}")//
+	private String windowTitle;
+        
+        @Autowired//
+	private MainLayout mainLayout;
+
+	@Override
+	public void start(Stage stage) throws Exception {
+
+		stage.setTitle(windowTitle);
+		stage.setScene(new Scene(mainLayout));
+		stage.setResizable(true);
+		stage.centerOnScreen();
+		stage.show();
+	}
+
+	public static void main(String[] args) {
+		launchApp(App.class, args);
+	}
+}
