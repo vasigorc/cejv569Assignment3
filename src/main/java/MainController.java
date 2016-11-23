b443db65da5355293bbb3e970ccde59e973bd492
vasigorc <vasigorc@yahoo.ca>
2016-11-23 07:12:15 -0500

    Adding controllers and views

---
+++ src/main/java/MainController.java
@@ -0,0 +1,21 @@
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author vgorcinschi
+ */
+public class MainController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
