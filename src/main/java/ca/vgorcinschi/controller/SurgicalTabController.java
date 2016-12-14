7669113bad201264264602a42f1df87b32e58aae
vgorcinschi <vgorcinschi@legal-suite.com>
2016-12-13 20:10:52 -0500

    SurgicalTab layout finished.

--- src/main/java/ca/vgorcinschi/controller/SurgicalTabController.java
+++ src/main/java/ca/vgorcinschi/controller/SurgicalTabController.java
@@ -1,8 +1,18 @@
 package ca.vgorcinschi.controller;
 
+import ca.vgorcinschi.dao.PatientDBService;
 import ca.vgorcinschi.model.Surgical;
+import ca.vgorcinschi.util.DozerMapper;
+import com.jfoenix.controls.JFXDatePicker;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.List;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 /**
@@ -11,43 +21,101 @@ import org.springframework.stereotype.Component;
  * @author vgorcinschi
  */
 @Component
-public class SurgicalTabController extends AbstractTabController <Surgical> implements Command {
+public class SurgicalTabController extends AbstractTabController<Surgical> implements Command {
 
+    //wired DAO
+    @Autowired
+    PatientDBService service;
+
+    //bean that copies object properties
+    @Autowired
+    DozerMapper dozerMapper;
+
+    private Surgical currentSurgical;
+
+    public Surgical getCurrentSurgical() {
+        return currentSurgical;
+    }
+
+    public void setCurrentSurgical(Surgical currentSurgical) {
+        this.currentSurgical = currentSurgical;
+    }
+
+    @FXML
+    TableView<Surgical> surgicalDataTable;
+    //data table columns
+    @FXML
+    TableColumn<Surgical, Number> idColumn;
+    @FXML
+    TableColumn<Surgical, LocalDateTime> surgicalDateColumn;
+    @FXML
+    TableColumn<Surgical, String> surgeryColumn;
+    @FXML
+    TableColumn<Surgical, BigDecimal> roomFeeColumn;
+    @FXML
+    TableColumn<Surgical, BigDecimal> surgeonFeeColumn;
+    @FXML
+    TableColumn<Surgical, BigDecimal> suppliesColumn;
     /**
      * Initializes the controller class.
      */
+    //Main view bindings
+    @FXML
+    TextField mvSurgicalID;
+    @FXML
+    TextField mvSurgery;
+    @FXML
+    TextField mvRoomFee;
+    @FXML
+    TextField mvSurgeonFee;
+    @FXML
+    TextField mvSuppliesFee;
+    @FXML
+    JFXDatePicker mvSurgicalDate;    
+    @FXML
+    JFXDatePicker mvSurgicalTime;    
+    @FXML
+    Button mvAddBtn;    
+    @FXML
+    Button mvDeleteBtn;    
+    @FXML
+    Button mvSaveBtn;    
+    @FXML
+    Button mvRewind;    
+    @FXML
+    Button mvForward;
     @FXML
     public void initialize() {
         // TODO
-    }    
+    }
 
     @Override
     public void execute() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
+
     @FXML
-    public void newSurgical(){
+    public void newSurgical() {
         //TODO
     }
-    
+
     @FXML
-    public void deleteSurgical(){
+    public void deleteSurgical() {
         //TODO
     }
-    
+
     @FXML
-    public void saveSurgical(){
+    public void saveSurgical() {
         //TODO
     }
-    
+
     @FXML
-    public void rewindSurgical(){
+    public void rewindSurgical() {
         //TODO
     }
-    
+
     @FXML
-    public void forwardSurgical(){
+    public void forwardSurgical() {
         //TODO
     }
 
